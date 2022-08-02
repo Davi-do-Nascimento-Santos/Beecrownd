89b5e3695145a9066c1d474e08992427519cc447
Davi do Nascimento Santos <davinascimento19072001@gmail.com>
2022-08-01 22:37:58 -0300

    Add more solutions

---
+++ rest_of_a_division.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	int x, y;
+	scanf("%d%d", &x, &y);
+	if (x<y){
+		x++;
+		for (x; x<y;  x++){
+			if (x%5==2 || x%5==3){
+				printf("%d\n", x);
+			}
+		}
+	} else {
+		y++;
+		for (y; y<x; y++){
+			if (y%5==2 || y%5==3){
+				printf("%d\n", y);
+			}
+		}
+	}
+	return 0;
+}
